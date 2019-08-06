0837785d50fa5365ca941f8901f97c76c967ad24
ajlopez <webmaster@ajlopez.com>
2019-08-06 10:45:26 -0300

    Parent in context

--- test/contexts.js
+++ test/contexts.js
@@ -15,3 +15,29 @@ exports['set and get variable'] = function (test) {
     test.equal(context.get('answer'), 42);
 };
 
+exports['get unknown variable in parent'] = function (test) {
+    const parent = contexts.context();
+    const context = contexts.context(parent);
+    
+    test.equal(context.get('foo'), null);
+};
+
+exports['get variable from parent'] = function (test) {
+    const parent = contexts.context();
+    parent.set('foo', 42);
+    
+    const context = contexts.context(parent);
+    
+    test.equal(context.get('foo'), 42);
+    test.equal(parent.get('foo'), 42);
+};
+
+exports['get variable from child'] = function (test) {
+    const parent = contexts.context();
+    
+    const context = contexts.context(parent);
+    context.set('foo', 42);
+    
+    test.equal(context.get('foo'), 42);
+    test.equal(parent.get('foo'), null);
+};
