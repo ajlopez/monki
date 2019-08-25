e51b7310e4085e1f5b4b09e925643d984c1b714d
ajlopez <webmaster@ajlopez.com>
2019-08-25 11:36:20 -0300

    Execute file

--- test/execute.js
+++ test/execute.js
@@ -2,11 +2,24 @@
 const contexts = require('../lib/contexts');
 const monki = require('../lib/monki');
 
-exports['execute command'] = function (test) {
+const path = require('path');
+
+exports['execute text'] = function (test) {
     const context = contexts.context();
     
     const result = monki.execute('let answer = 42;', context);
     
+    test.equal(context.get('answer'), 42);
+};
+
+exports['execute file'] = function (test) {
+    const filename = path.join(__dirname, 'files', 'Let.mon')
+    const context = contexts.context();
+    
+    const result = monki.executeFile(filename, context);
+    
+    test.equal(context.get('one'), 1);
+    test.equal(context.get('two'), 2);
     test.equal(context.get('answer'), 42);
 };
 
