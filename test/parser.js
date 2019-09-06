ba05089dd6046bf8fbf8a4a453a24ee290da07e2
ajlopez <webmaster@ajlopez.com>
2019-09-06 07:46:28 -0300

    Parse unary operators

--- test/parser.js
+++ test/parser.js
@@ -9,7 +9,6 @@ function parse(test, type, text, expected) {
 
 exports['parse constants'] = function (test) {
     parse(test, 'integer', '42', { ntype: 'constant', value: 42 });
-    parse(test, 'integer', '-3', { ntype: 'constant', value: -3 });
     parse(test, 'string', '"foo"', { ntype: 'constant', value: 'foo' });
     parse(test, 'boolean', 'true', { ntype: 'constant', value: true });
     parse(test, 'boolean', 'false', { ntype: 'constant', value: false });
@@ -21,6 +20,11 @@ exports['parse terms'] = function (test) {
     parse(test, 'term', 'true', { ntype: 'constant', value: true });
     parse(test, 'term', 'false', { ntype: 'constant', value: false });
     parse(test, 'term', 'answer', { ntype: 'name', name: 'answer' });
+};
+
+exports['parse expressions with unary operator'] = function (test) {
+    parse(test, 'expression', '!true', { ntype: 'unary', operator: '!', expression: { ntype: 'constant', value: true } });
+    parse(test, 'expression', '-42', { ntype: 'unary', operator: '-', expression: { ntype: 'constant', value: 42 } });
 };
 
 exports['parse arithmetic expressions'] = function (test) {
